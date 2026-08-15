2964d7d65b7306ed5499fdb5aaf1a05f8389631d
Adebola Shopeju <sadebola@nmsu.edu>
2026-08-15 23:26:15 +0100

    W9-D5: Integration --- connected Fargate app to RDS MySQL end to end

--- week9/labs/W9-D1/app/app.py
+++ week9/labs/W9-D1/app/app.py
@@ -1,1 +1,10 @@
-print("Second version!")
+import os, pymysql
+conn = pymysql.connect(
+    host=os.environ['DB_HOST'],
+    user=os.environ['DB_USER'],
+    password=os.environ['DB_PASS'],
+    database=os.environ['DB_NAME'])
+with conn.cursor() as cur:
+    cur.execute("SELECT * FROM proof")
+    print("READ FROM RDS:", cur.fetchall())
+conn.close()
